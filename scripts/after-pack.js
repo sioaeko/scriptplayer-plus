a535cecb6214f64f86968c1771e67cdef98de03d
sioaeko <grade0422@gmail.com>
2026-05-24 22:54:13 +0900

    Sign mac app bundles during packaging

--- scripts/after-pack.js
+++ scripts/after-pack.js
@@ -1,7 +1,27 @@
 const path = require('path')
+const { execFileSync } = require('child_process')
 const { updateExecutableIcon } = require('./set-icon')
 
 exports.default = async function afterPack(context) {
+  if (context.electronPlatformName === 'darwin') {
+    const appName = `${context.packager.appInfo.productFilename}.app`
+    const appPath = path.join(context.appOutDir, appName)
+
+    execFileSync('codesign', ['--force', '--deep', '--sign', '-', appPath], {
+      stdio: 'inherit',
+    })
+
+    execFileSync(
+      'codesign',
+      ['--verify', '--deep', '--strict', '--verbose=2', appPath],
+      {
+        stdio: 'inherit',
+      },
+    )
+
+    return
+  }
+
   if (context.electronPlatformName !== 'win32') {
     return
   }
